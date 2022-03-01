8fdb23c317b239a14cf86b2d60c74175255024b7
william-hopkins <will.hopkins@tile.com>
2022-03-01 10:35:44 -0500

    Add first dotfiles

---
+++ dotfiles/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
---
+++ dotfiles/.config/fish/functions/gm.fish
@@ -0,0 +1,6 @@
+function gm
+git -C ~/Code/tile_data_engineering pull;
+git -C ~/Code/aws-managed-airflow-dags pull;
+and brew update;
+and brew upgrade
+end
